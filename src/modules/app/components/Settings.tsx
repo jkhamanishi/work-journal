8f3787a069a0896f5e86d381fbcc7f66c152315e
Jackson Hamanishi <jkhamanishi@gmail.com>
2025-01-05 16:48:53 -0800

    Added preset themes

--- src/modules/app/components/Settings.tsx
+++ src/modules/app/components/Settings.tsx
@@ -13,7 +13,14 @@ function TextSetting({label, defaultValue, changeFcn}: {label: string, defaultVa
     changeFcn(event.currentTarget.value);
   }
   return (
-    <label>{label}: <input type="text" defaultValue={defaultValue} onChange={onChange} /></label>
+    <label>{label}: <input type="text" value={defaultValue} onChange={onChange} /></label>
+  );
+}
+
+function ThemeOption({textColor, bgColor, setTheme, name}: {textColor: string, bgColor: string, setTheme: (tc: string, bc: string) => void, name: string}) {
+  const onClick = () => setTheme(textColor, bgColor);  
+  return (
+    <button onClick={onClick}>{name}</button>
   );
 }
 
@@ -21,7 +28,7 @@ function Settings() {
   const [show, setShow] = useState(false);
   const toggleShow = () => setShow(show => !show);
   
-  const [textColor, setTextColor] = useState("");
+  const [textColor, setTextColor] = useState("#11c700");
   const changeTextColor = (color: string) => {
     const root = document.getElementById("root")!;
     root.style.setProperty('--text-color', color);
@@ -29,12 +36,17 @@ function Settings() {
     setTextColor(color);
   }
   
-  const [bgColor, setBgColor] = useState("");
+  const [bgColor, setBgColor] = useState("#000000");
   const changeBgColor = (color: string) => {
     const root = document.getElementById("root")!;
     root.style.setProperty('--bg-color', color);
     localStorage.setItem("bg-color", color);
     setBgColor(color);
+  }
+  
+  const setTheme = (themeTextColor: string, themeBgColor: string) => {
+    changeTextColor(themeTextColor);
+    changeBgColor(themeBgColor);
   }
   
   useEffect(() => {
@@ -46,7 +58,15 @@ function Settings() {
     <>
       <button onClick={toggleShow}>Settings</button>
       {show && (
-        <div className="settings">
+        <div id="settings">
+          <div className="section-title">Settings</div>
+          <div id="preset-themes">
+            <div style={{marginBottom: "0.5rem"}}>Preset themes: </div>
+            <ThemeOption name="Hacker (default)" textColor="#11c700"   bgColor="#000000" setTheme={setTheme}></ThemeOption>
+            <ThemeOption name="Dev's Theme"      textColor="#FF75CA"   bgColor="#550034" setTheme={setTheme}></ThemeOption>
+            <ThemeOption name="Noir"             textColor="lightgray" bgColor="black"   setTheme={setTheme}></ThemeOption>
+            <ThemeOption name="Hotdog Stand"     textColor="yellow"    bgColor="red"     setTheme={setTheme}></ThemeOption>
+          </div>
           <TextSetting label="Text color" defaultValue={textColor} changeFcn={changeTextColor}/>
           <TextSetting label="Background color" defaultValue={bgColor} changeFcn={changeBgColor}/>
         </div>
