1a1fb8e9378a3d737a247535b54381c6ee29db1f
Jackson Hamanishi <jkhamanishi@gmail.com>
2025-02-05 20:23:45 -0800

    Declared ViteSVGElement as a global type

--- src/@types/svg.d.ts
+++ src/@types/svg.d.ts
@@ -1,14 +1,10 @@
 // Derived from vite-plugin-svgr/client.d.ts
 
-type ViteSVGElement = React.FunctionComponent<
+declare type ViteSVGElement = React.FunctionComponent<
   React.ComponentProps<"svg"> & { title?: string }
 >;
 
 declare module "*.svg" {
   const ReactComponent: ViteSVGElement
   export default ReactComponent;
-}
-
-declare module "vite-svg" {
-  export default ViteSVGElement;
 }
